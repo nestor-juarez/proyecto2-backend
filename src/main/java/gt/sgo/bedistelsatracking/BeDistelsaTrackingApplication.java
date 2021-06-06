690334257ed6b3b2c68b919576e5cca54cd1e8ba
Darwyn Hernandez <darwyn001@gmail.com>
2021-06-05 21:20:12 -0600

    Initial Commit

---
+++ src/main/java/gt/sgo/bedistelsatracking/BeDistelsaTrackingApplication.java
@@ -0,0 +1,13 @@
+package gt.sgo.bedistelsatracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDistelsaTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDistelsaTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gt/sgo/bedistelsatracking/BeDistelsaTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.sgo.bedistelsatracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDistelsaTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
